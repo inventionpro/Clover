92355a9128c88bb1199fc8dce072f0ffba10b0dd
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-04-24 23:41:16 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+const filep = document.getElementById('file');
+
+function view() {
+  let file = filep.files[0];
+  if (file) {
+    let reader = new FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onload = function (evt) {
+      ctx.clearRect(0, 0, canvas.width, canvas.height)
+      let data = new Uint8Array(evt.target.result);
+
+      if (data.slice(0,4).join('') !== '') {
+        alert('Invalid file contents')
+        return;
+      }
+      canvas.width = parseInt(Array.from(data.slice(6,10)).map(e=>e.toString(2).padStart(8, '0')).join(''), 2);
+      canvas.height = parseInt(Array.from(data.slice(10,14)).map(e=>e.toString(2).padStart(8, '0')).join(''), 2);
+    }
+    reader.onerror = function (evt) {
+      alert("Error reading file");
+    }
+  }
+}
