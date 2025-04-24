dc2b0e8cbac2f062c8c0010d52db62f9e073fa76
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-04-25 00:09:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,22 +5,41 @@ const filep = document.getElementById('file');
 
 function view() {
   let file = filep.files[0];
-  if (file) {
-    let reader = new FileReader();
-    reader.readAsArrayBuffer(file);
-    reader.onload = function (evt) {
-      ctx.clearRect(0, 0, canvas.width, canvas.height)
-      let data = new Uint8Array(evt.target.result);
+  if (!file) return;
 
-      if (data.slice(0,4).join('') !== '') {
-        alert('Invalid file contents')
-        return;
-      }
-      canvas.width = parseInt(Array.from(data.slice(6,10)).map(e=>e.toString(2).padStart(8, '0')).join(''), 2);
-      canvas.height = parseInt(Array.from(data.slice(10,14)).map(e=>e.toString(2).padStart(8, '0')).join(''), 2);
+  let reader = new FileReader();
+  reader.readAsArrayBuffer(file);
+  reader.onload = function (evt) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    let data = new Uint8Array(evt.target.result);
+
+   if (data.slice(0,4).join('') !== '') {
+      alert('Invalid file contents')
+      return;
     }
-    reader.onerror = function (evt) {
-      alert("Error reading file");
-    }
+    canvas.width = parseInt(Array.from(data.slice(6,10)).map(e=>e.toString(2).padStart(8, '0')).join(''), 2);
+    canvas.height = parseInt(Array.from(data.slice(10,14)).map(e=>e.toString(2).padStart(8, '0')).join(''), 2);
+  }
+  reader.onerror = function (evt) {
+    alert("Error reading file");
   }
 }
+
+function encode() {
+  let file = filep.files[0];
+  if (!file) return;
+
+  let url = URL.createObjectURL(file);
+  let img = new Image();
+  img.onload = function() {
+    URL.revokeObjectURL(url);
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    ctx.drawImage(img, 0, 0);
+
+    let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+    let newimg = new UInt8Array();
+  };
+  img.src = url;
+});
