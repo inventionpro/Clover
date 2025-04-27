6d35957e3abe9766d6d7356dfee3f8c6803765eb
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-04-27 15:26:26 +0200

    Use module

--- media/script.js
+++ media/script.js
@@ -4,41 +4,10 @@ const ctx = canvas.getContext('2d');
 const filep = document.getElementById('file');
 
 function view() {
-  let file = filep.files[0];
-  if (!file) return;
-
-  let reader = new FileReader();
-  reader.readAsArrayBuffer(file);
-  reader.onload = function (evt) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    let data = new Uint8Array(evt.target.result);
-
-   if (data.slice(0,4).join('') !== '240159141128') {
-      alert('Invalid file contents')
-      return;
-    }
-    let ver = data.slice(4,5);
-    canvas.width = parseInt(Array.from(data.slice(5,7)).map(e=>e.toString(2).padStart(8, '0')).join(''), 2)*2;
-    canvas.height = parseInt(Array.from(data.slice(7,9)).map(e=>e.toString(2).padStart(8, '0')).join(''), 2)*2;
-
-    let datasec = data.slice(9);
-    let pixels = [];
-    for (let i = 0; i<canvas.width*canvas.height*4; i++) {
-      if (!pixels[Math.floor(i/4)]) pixels[Math.floor(i/4)] = [];
-      pixels[Math.floor(i/4)][i%4] = datasec[i];
-    }
-
-    for (let b = 0; b<4; b++) {
-      for (let i = 0; i<canvas.width*canvas.height/4; i++) {
-        let x = (i%(canvas.width/2))*2+b%2;
-        let y = Math.floor(i/(canvas.width/2))*2+Math.floor(b/2);
-        ctx.fillStyle = `rgba(${pixels[i][0]}, ${pixels[i][1]}, ${pixels[i][2]}, ${pixels[i][3]/255})`;
-        ctx.fillRect(x+1, y+1, 1, 1);
-      }
-    }
-  }
-  reader.onerror = function (evt) {
-    alert("Error reading file");
+  try {
+    Clover.loadFromFile(filep.files[0], canvas);
+  } catch(err) {
+    alert(err);
   }
 }
 
@@ -74,7 +43,7 @@ function encode() {
         datasec[idx+3] = imageData[pidx+3]??0;
       }
     }
-    
+
     let newimg = new Uint8Array(datasec.length+16);
     newimg.set([240, 159, 141, 128]); // Magic 🍀
     newimg.set([128], 4); // Version 0 with alpha
