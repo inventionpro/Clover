90daf5b715461eb46e44734b644f53a7043db36a
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-04-27 15:47:57 +0200

    Update module.js

--- module.js
+++ module.js
@@ -41,6 +41,62 @@ let Clover = {
       throw new Error('Cannot read file');
     }
     reader.readAsArrayBuffer(file);
+  },
+  convertFromFile: (file)=>{
+    if (!file) return;
+
+    let canvas = document.createElement('canvas');
+    let ctx = canvas.getContext('2d');
+    let url = URL.createObjectURL(file);
+    let img = new Image();
+    img.onload = function() {
+      URL.revokeObjectURL(url);
+      canvas.width = Math.ceil(img.width/2)*2;
+      canvas.height = Math.ceil(img.height/2)*2;
+  
+      ctx.drawImage(img, 0, 0);
+  
+      let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+  
+      let blockWidth = canvas.width / 2;
+      let blockHeight = canvas.height / 2;
+      let datasec = new Uint8Array(blockWidth * blockHeight * 4);
+  
+      for (let blockY = 0; blockY < blockHeight; blockY++) {
+        for (let blockX = 0; blockX < blockWidth; blockX++) {
+          let x = 1 + blockX * 2;
+          let y = 1 + blockY * 2;
+          let pidx = (y * canvas.width + x) * 4;
+  
+          const idx = (blockY * blockWidth + blockX) * 4;
+          datasec[idx] = imageData[pidx]??0;
+          datasec[idx+1] = imageData[pidx+1]??0;
+          datasec[idx+2] = imageData[pidx+2]??0;
+          datasec[idx+3] = imageData[pidx+3]??0;
+        }
+      }
+      
+      let newimg = new Uint8Array(datasec.length+16);
+      newimg.set([240, 159, 141, 128]); // Magic 🍀
+      newimg.set([128], 4); // Version 0 with alpha
+      newimg.set([(canvas.width/2)>>8, (canvas.width/2)&255], 5); // Width
+      newimg.set([(canvas.height/2)>>8, (canvas.height/2)&255], 7); // Height
+      newimg.set(datasec, 9);
+  
+      let blob = new Blob([newimg], { type: 'image/clover' });
+      return blob;
+    };
+    img.src = url;
+  },
+  downloadFile: (file, name)=>{
+    let newurl = URL.createObjectURL(file);
+
+    let a = document.createElement('a');
+    a.href = newurl;
+    a.download = name;
+    a.click();
+
+    URL.revokeObjectURL(newurl);
   }
 };
 
